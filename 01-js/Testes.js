26fff4b57fc5f094e46db398e0c00565ddb5f01e
camillacord <camilla.cordeiro@icloud.com>
2026-04-16 22:10:27 -0300

    atualização

---
+++ 01-js/Testes.js
@@ -0,0 +1,18 @@
+// // alert ("Olá mundo!")
+// // alert ("Olá Olá")
+// let nome 
+
+// nome = "camilla"
+
+// // alert ("meu nome é " + nome)
+// let idade = 21 
+// // alert (" idade: "+ idade + " meu nome é " + nome)
+// nome = prompt("digite seu nome:")
+// alert("O nome digitado foi:"+ nome)
+
+
+let nota1 
+nota1 = prompt("Digite sua primeira nota aqui: ")
+nota1= Number()
+nota1= nota1+1
+alert(nota1)
